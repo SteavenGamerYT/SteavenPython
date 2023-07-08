ee46feb33afb5660eb1ca7b4708388c1cf47a04a
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2023-07-08 15:23:48 +0300

    Update Harm-Halal.py

--- Harm-Halal.py
+++ Harm-Halal.py
@@ -1,8 +1,9 @@
 print('Hello, world!')
 haramhalal = input("Are you Haram or Halal? ").lower()
+
 if haramhalal == 'haram':
     print("Haram, you Frinda!")
 elif haramhalal == 'halal':
     print("Halal, you Frinda!")
 else:
-    print("Invalid input. Please choose between Haram and Halal.")
+    print("Please choose between Haram and Halal.")
